d384118397ea5831ed2b27864650b249ff12aad9
Akshay <akshayyadav04031998@gmail.com>
2021-05-24 22:39:50 +0530

    "Meal item form frontend completed"

--- src/Components/Meals/MealItem.js
+++ src/Components/Meals/MealItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Styles from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 const MealItem = (props) => {
   return (
     <li className={Styles.meal}>
@@ -8,7 +9,9 @@ const MealItem = (props) => {
         <div className={Styles.description}>{props.mealdescription}</div>
         <div className={Styles.price}>{`$${props.mealprice.toFixed(2)}`}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
