899b23d53bee54e720d71a159659b98852f3a8e2
Akshay <akshayyadav04031998@gmail.com>
2021-05-24 16:25:30 +0530

    "Meals Completed"

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,2 +1,38 @@
-const AvailableMeals = () => {};
+import Styles from "./AvailableMeals.module.css";
+
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
+
+const AvailableMeals = () => {
+  const mealslist = DUMMY_MEALS.map((meal) => <section>{meal.name}</section>);
+  return (
+    <section className={Styles.meals}>
+      <ul>{mealslist}</ul>
+    </section>
+  );
+};
 export default AvailableMeals;
