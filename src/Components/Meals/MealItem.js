f82f62d3db12ecd6ea312c24429b35b155bef8fa
Akshay <akshayyadav04031998@gmail.com>
2021-05-24 17:55:06 +0530

    "MealItem Completed"

--- src/Components/Meals/MealItem.js
+++ src/Components/Meals/MealItem.js
@@ -2,11 +2,14 @@ import React from "react";
 import Styles from "./MealItem.module.css";
 const MealItem = (props) => {
   return (
-    <div className={Styles.meal}>
-      <h3>{props.mealname}</h3>
-      <div className={Styles.price}>{props.mealprice}</div>
-      <div className={Styles.description}>{props.mealdescription}</div>
-    </div>
+    <li className={Styles.meal}>
+      <div>
+        <h3>{props.mealname}</h3>
+        <div className={Styles.description}>{props.mealdescription}</div>
+        <div className={Styles.price}>{`$${props.mealprice.toFixed(2)}`}</div>
+      </div>
+      <div></div>
+    </li>
   );
 };
 export default MealItem;
