cfa7c45bc158d460398b2bec07a70138921df08d
Akshay <akshayyadav04031998@gmail.com>
2021-06-22 09:56:24 +0530

    Adding style to form and reading form values

--- src/Components/Cart/Checkout.js
+++ src/Components/Cart/Checkout.js
@@ -1,34 +1,49 @@
+import { useRef } from "react";
 import Styles from "./Checkout.module.css";
 const Checkout = (props) => {
+  const nameRef = useRef();
+  const mobileRef = useRef();
+  const streetRef = useRef();
+  const cityRef = useRef();
+  const postalRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
+    const enteredName = nameRef.current.value;
+    const enteredMobile = mobileRef.current.value;
+    const enteredStreet = streetRef.current.value;
+    const enteredCity = cityRef.current.value;
+    const enteredPostalCode = postalRef.current.value;
   };
+
   return (
-    <form onSubmit={confirmHandler}>
+    <form className={Styles.form} onSubmit={confirmHandler}>
       <div className={Styles.control}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="namw" />
+        <input type="text" id="namw" ref={nameRef} />
       </div>
       <div className={Styles.control}>
         <label htmlFor="mobile">Mobile</label>
-        <input type="text" id="mobile" />
+        <input type="text" id="mobile" ref={mobileRef} />
       </div>
       <div className={Styles.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
+        <input type="text" id="street" ref={streetRef} />
       </div>
       <div className={Styles.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityRef} />
       </div>
       <div className={Styles.control}>
         <label htmlFor="postalcode">Postal Code</label>
-        <input type="text" id="postalcode" />
+        <input type="text" id="postalcode" ref={postalRef} />
       </div>
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={Styles.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={Styles.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
